5fd9f89bc0161e626b40ede3b2d91228bc9969a9
Ninglo <jiujianian@gmail.com>
2024-07-20 17:04:38 +0800

    🎉 Hello Quick Voice Note!

---
+++ README.md
@@ -0,0 +1,17 @@
+# Quick Voice Note
+
+> Start taking voice notes with one shortcut (`cmd (ctrl) + shift + v`) ! 
+
+## Show case
+
+![Show case](./resource/show_case.gif)
+
+## Extra features
+
+Use `Open Created Notes` to view your previous notes.
+
+![Open Created Notes](./resource/image.png)
+
+Also, you can change the notes' storage directory by changing `quick-voice-note.noteFolder` in your `settings.json`.
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+import * as vscode from 'vscode';
+
+function getConfigProperty(property: 'noteFolder', fallback: string): string;
+function getConfigProperty<T>(property: string, fallback: T): T {
+	const config = vscode.workspace.getConfiguration('quick-voice-note');
+	return config.get(property, fallback);
+};
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('quick-voice-note.takeNote', async () => {
+		const configPath = getConfigProperty('noteFolder', '');
+		const notesDirPath = configPath ? vscode.Uri.file(configPath) : context.globalStorageUri;
+		const noteFilePath = await findValidPath(notesDirPath)(1);
+
+		await vscode.workspace.fs.createDirectory(notesDirPath);
+		await vscode.workspace.fs.writeFile(noteFilePath, new Uint8Array(0));
+		await vscode.workspace.openTextDocument(noteFilePath).then(vscode.window.showTextDocument);
+
+		await vscode.commands.executeCommand('workbench.action.editorDictation.start');
+	});
+
+	const disposable2 = vscode.commands.registerCommand('quick-voice-note.openNote', async () => {
+		const configPath = getConfigProperty('noteFolder', '');
+		const notesDirPath = configPath ? vscode.Uri.file(configPath) : context.globalStorageUri;
+		const noteFiles = await vscode.workspace.fs.readDirectory(notesDirPath);
+		const fileNames = noteFiles.map(([fileName]) => fileName);
+
+		const selectedFileName = await vscode.window.showQuickPick(fileNames, {
+			title: 'Select a note file',
+			placeHolder: 'Select a note file to open',
+		});
+		if (!selectedFileName) {
+			return;
+		}
+
+		const noteFilePath = vscode.Uri.joinPath(notesDirPath, selectedFileName);
+		await vscode.workspace.openTextDocument(noteFilePath).then(vscode.window.showTextDocument);
+	});
+
+	context.subscriptions.push(disposable, disposable2);
+}
+
+const findValidPath = (dir: vscode.Uri, template = 'note-${index}.md') => (index: number): Thenable<vscode.Uri> => {
+	const noteFileName = template.replace('${index}', index.toString());
+	const noteFilePath = vscode.Uri.joinPath(dir, noteFileName);
+	return vscode.workspace.fs.stat(noteFilePath).then(
+		() => findValidPath(dir, template)(index + 1),
+		() => noteFilePath
+	);
+};
+
+export function deactivate() { }
